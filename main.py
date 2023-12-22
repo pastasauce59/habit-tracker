06d9367400331445df91436d6fafc81bc4ebe709
Michael Primavera <pastasauce59@gmail.com>
2023-12-21 23:44:19 -0500

    Created pixela account using post request.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Creating a habit tracking app using Pixela API
-
+from keys import *
 import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
-requests.put()
+user_params = {
+    "token": pixela_token,
+    "username": pixela_username,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+## Pixela Account Succefully created
+# response = requests.post(url=pixela_endpoint, json=user_params )
+# print(response.text)
 
