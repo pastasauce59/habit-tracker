de71ad221468f095c5e7137f0a1bbb1d8cf3a4ec
Michael Primavera <pastasauce59@gmail.com>
2023-12-22 00:21:38 -0500

    Successfully posted to graph using pixela entry endpoint and post request.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+headers = {
+    "X-USER-TOKEN": pixela_token
+}
+
 user_params = {
     "token": pixela_token,
     "username": pixela_username,
@@ -11,24 +15,32 @@ user_params = {
     "notMinor": "yes"
 }
 
-## Pixela Account Succefully created
 # response = requests.post(url=pixela_endpoint, json=user_params )
 # print(response.text)
+## Pixela Account Succefully created
 
-#Graph endpoint
-graph_endpoint = f"{pixela_endpoint}/{pixela_username}/graphs"
+##Graph endpoint
 
-graph_configuration ={ 
-    "id": "graph1",
-    "name": "Language Practice",
-    "unit": "minutes",
-    "type": "int",
-    "color": "sora"
+# graph_endpoint = f"{pixela_endpoint}/{pixela_username}/graphs"
+
+# graph_configuration ={ 
+#     "id": "graph1",
+#     "name": "Language Practice",
+#     "unit": "minutes",
+#     "type": "int",
+#     "color": "sora"
+# }
+
+# response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
+# print(response.text)
+## Graph Successfully Created
+
+entry_endpoint = f"{pixela_endpoint}/{pixela_username}/graphs/graph1"
+
+entry_configuration = {
+    "date": "20231222",
+    "quantity": "10"
 }
 
-headers = {
-    "X-USER-TOKEN": pixela_token
-}
-
-response = requests.post(url=graph_endpoint, json=graph_configuration, headers=headers)
+response = requests.post(url=entry_endpoint, json=entry_configuration, headers=headers)
 print(response.text)
